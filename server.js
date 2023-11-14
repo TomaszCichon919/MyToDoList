0762355b3bf0db2ed8ed06415b7493d79ef4e8c7
Tomasz Cichon <tomekcichon@tlen.pl>
2023-11-14 14:43:29 +0100

    code format

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
-  });
+});
 
 const io = socket(server);
 
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
     console.log('New client! Its id – ' + socket.id);
     console.log(tasks);
     io.to(socket.id).emit('updateData', JSON.stringify(tasks));
-    
+
     socket.on('addTask', (task) => {
         console.log('Oh, I\'ve got something from ' + socket.id);
         tasks.push(task);
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 
     socket.on('removeTask', (taskId) => {
         const indexToRemove = tasks.findIndex((task) => task.id === taskId);
-    
+
         if (indexToRemove !== -1) {
             const removedTask = tasks.splice(indexToRemove, 1)[0];
             console.log('Removed task:', removedTask);
@@ -41,15 +41,15 @@ io.on('connection', (socket) => {
     socket.on('editTask', ({ id, newName }) => {
         const taskToEdit = tasks.find((task) => task.id === id);
         if (taskToEdit) {
-          taskToEdit.name = newName;
-          socket.broadcast.emit('editTask', { id, newName });
+            taskToEdit.name = newName;
+            socket.broadcast.emit('editTask', { id, newName });
         }
-      });
+    });
 
 });
 
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
-  });
-  
+});
+
